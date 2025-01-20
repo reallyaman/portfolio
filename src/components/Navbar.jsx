b7a60ba7f505cea6db17d70f63e9e73d94c3b79b
amanpandey <amanpandey143182002@gmail.com>
2025-01-20 14:32:57 +0530

    fourthh commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import logo from '../assets/ApLogo.png';
-import { FaLinkedin } from 'react-icons/fa';
+import logo from "../assets/ApLogo.png";
+import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -11,13 +11,30 @@ function Navbar() {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className=" flex items-center justify-center gap-4 m-8 text-2xl">
-        <FaLinkedin className=" hover:text-[#0077B5]" />
-        <FaGithub className=" hover:text-[#fafbfc]" />
-        <FaInstagram className=" hover:text-[#c13584]" />
-        <FaSquareXTwitter className=" hover:text-[#000000] hover:bg-white" />
+        <a
+          href="https://www.linkedin.com/in/aman-pandey-275431203"
+          target="_blank"
+        >
+          <FaLinkedin className=" hover:text-[#0077B5]" />
+        </a>
+
+        <a
+          href="https://github.com/reallyaman?tab=repositories"
+          target="_blank"
+        >
+          <FaGithub className=" hover:text-[#fafbfc]" />
+        </a>
+
+        <a href="https://www.instagram.com/reallyamann/#" target="_blank">
+          <FaInstagram className=" hover:text-[#c13584]" />
+        </a>
+
+        <a href="">
+          <FaSquareXTwitter className=" hover:text-[#000000] hover:bg-white" />
+        </a>
       </div>
     </nav>
   );
 }
 
-export default Navbar
+export default Navbar;
